6a303ac77219f822ca015044cbd08096d423923a
kutiturkes <46580012+kutiturkes@users.noreply.github.com>
2026-07-07 23:02:36 -0400

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,16 @@
+window.DPS_CONFIG = {
+  businessName: "Dayton Property Services",
+  phoneDisplay: "(812) 259-3490",
+  phoneHref: "+18122593490",
+  email: "info@daytonpropertyservice.com",
+  serviceAreas: [
+    "Dayton",
+    "Kettering",
+    "Beavercreek",
+    "Centerville",
+    "Huber Heights",
+    "Fairborn",
+    "Vandalia",
+    "Englewood"
+  ]
+};
